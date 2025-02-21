f97e2e0812fec5449e295aeefa012349a8e03373
Chase <chasepen08@gmail.com>
2025-02-21 18:10:43 +0000

    Add app.py to my project

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask
-from flask import render_template
-from datetime import date
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-def todays_date():
-	today = date.today()
-	str_date = today.strftime("%B %d %Y")
-	return "Today's date is " + str_date
-
 @app.route("/")
-def hello_world():
-	today = todays_date()
-	return render_template("index.html")
+def home():
+    return "<h1>Welcome to My Website</h1><p><a href='/resume'>View My Resume</a></p><p><a href='/about'>About Me</a></p>"
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
 @app.route("/about")
-def about_me():
-	return render_template("about_me.html")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
